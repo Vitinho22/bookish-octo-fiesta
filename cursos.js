13585f03fbb2685c29925ce069b380dc93791327
Vitinho22 <78907979+Vitinho22@users.noreply.github.com>
2024-10-29 01:03:24 -0300

    Add files via upload

---
+++ cursos.js
@@ -0,0 +1,33 @@
+// Array de cursos
+export const cursos = [
+    {
+        nome: "Curso de JavaScript",
+        descricao: "Aprenda JavaScript do zero ao avançado.",
+        imagem: "https://via.placeholder.com/250x150",
+        link: "#"
+    },
+    {
+        nome: "Curso de Hambúrguer Artesanal",
+        descricao: "Construa páginas web profissionais.",
+        imagem: "imagens/CapaHotmartHamburguerArtesanal.webp",
+        link: "https://www.cursodehamburguer.com/oferta149/?ref=M96031928W"
+    },
+    {
+        nome: "Curso de React",
+        descricao: "Desenvolva interfaces modernas com React.",
+        imagem: "https://via.placeholder.com/250x150",
+        link: "#"
+    },
+    {
+        nome: "Curso de Node.js",
+        descricao: "Aprenda desenvolvimento back-end com Node.js.",
+        imagem: "https://via.placeholder.com/250x150",
+        link: "#"
+    },
+    {
+        nome: "Curso de Node.js",
+        descricao: "Aprenda desenvolvimento back-end com Node.js.",
+        imagem: "https://via.placeholder.com/250x150",
+        link: "#"
+    }
+];
